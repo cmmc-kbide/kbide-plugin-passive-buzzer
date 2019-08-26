c39fec87be722f03be8ca802b167a723ab7ebe6d
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 12:53:36 +0700

    add generator

--- blocks/blocks_passive-buzzer.js
+++ blocks/blocks_passive-buzzer.js
@@ -1,15 +1,22 @@
-Blockly.Blocks['ky_006_passive_buzzer_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-006 PASSIVE BUZZER");
-    this.appendValueInput("KY_006_PASSIVE_BUZZER_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_006_passive_buzzer_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-006 PASSIVE BUZZER");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(
+          new Blockly.FieldTextInput("0"),
+          "KY_006_PASSIVE_BUZZER_PIN"
+        )
+        .appendField("SET STATE")
+        .appendField(
+          new Blockly.FieldVariable("KY_006_PASSIVE_BUZZER_STATE"),
+          "KY_006_PASSIVE_BUZZER_STATE"
+        );
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_passive-buzzer.js
+++ blocks/generators_passive-buzzer.js
@@ -1,6 +1,24 @@
-Blockly.JavaScript['ky_006_passive_buzzer_block'] = function(block) {
-  var value_ky_006_passive_buzzer_pin = Blockly.JavaScript.valueToCode(block, 'KY_006_PASSIVE_BUZZER_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
+module.exports = function(Blockly) {
+  Blockly.JavaScript["ky_006_passive_buzzer_block"] = function(block) {
+    var text_ky_006_passive_buzzer_pin = block.getFieldValue(
+      "KY_006_PASSIVE_BUZZER_PIN"
+    );
+    var variable_ky_006_passive_buzzer_state = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_006_PASSIVE_BUZZER_STATE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#VARIABLE
+		int KY_006_PASSIVE_BUZZER_PIN = ${text_ky_006_passive_buzzer_pin}; // set the buzzer control digital IO pin
+	#END
+
+	#SETUP
+		pinMode(KY_006_PASSIVE_BUZZER_PIN, OUTPUT); // set pin 8 as output
+	#END
+
+	${variable_ky_006_passive_buzzer_state} = ${text_ky_006_passive_buzzer_pin};
+    `;
+    return code;
+  };
 };
